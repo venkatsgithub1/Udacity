0d4250800bec9777d3f960f96ad3cdd94507f583
venkatsgithub1 <venkatsgithub@gmail.com>
2017-03-11 21:35:13 +0530

    Create index.py
    A small example on index operation of list.

---
+++ lists/index.py
@@ -0,0 +1,5 @@
+list1=[1,2,3]
+#below line prints out 1, 1 is the index in list list1 where value 2 is present.
+print (list1.index(2))
+#below line gives out error since 5 is not present in the list.
+print (list1.index(5))
