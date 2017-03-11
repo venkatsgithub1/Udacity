dd0d76fe477263d568cd75ef1bba07967d19ebeb
venkatsgithub1 <venkatsgithub@gmail.com>
2017-03-11 21:39:59 +0530

    Create in_keyword.py
    A small example on using in keyword with lists to find elements in the list.

---
+++ lists/in_keyword.py
@@ -0,0 +1,11 @@
+list1=[1,2,3,4]
+
+varToBeFound=2
+
+#below line prints True because value 2 is present in  list1
+print (varToBeFound in list1)
+
+varToBeFound=55
+
+#below line prints False since 55 is not present in list1
+print (varToBeFound in list1)
